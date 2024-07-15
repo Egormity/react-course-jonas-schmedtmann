88d5e6bde5d10842dc534e64bad36405dca50215
Egormity <kotlaregor9@gmail.com>
2024-07-15 21:32:22 +0700

    wild oasis started

---
+++ 015-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tqlpeeginxvocmwxjcur.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxbHBlZWdpbnh2b2Ntd3hqY3VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwMjAwODMsImV4cCI6MjAzNjU5NjA4M30.rIVhC6DgZbsRatBCWhiIGDi31lL7PT9x_LOc2SOJ88A';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
