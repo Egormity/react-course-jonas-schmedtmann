17ca01510c27992ac0b07394a22744015b773265
Egormity <kotlaregor9@gmail.com>
2024-07-16 16:42:45 +0700

    React Query

--- 015-the-wild-oasis/src/services/apiCabins.js
+++ 015-the-wild-oasis/src/services/apiCabins.js
@@ -1,12 +1,28 @@
 import supabase from './supabase';
 
 export async function getCabins() {
-  let { data, error } = await supabase.from('cabins').select('*');
-
+  const { data, error } = await supabase.from('cabins').select('*');
   if (error) {
     console.log(`!!! ${error} !!!`);
-    throw new Error(`!!! Cabins couldn't be loaded !!!`);
+    throw new Error(`📛 Cabins could not be loaded`);
   }
+  return data;
+}
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase.from('cabins').insert([newCabin]).select();
+  if (error) {
+    console.log(`!!! ${error} !!!`);
+    throw new Error(`📛 Cabin could not be created`);
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from('cabins').delete().eq('id', id);
+  if (error) {
+    console.log(`!!! ${error} !!!`);
+    throw new Error(`📛 Cabin could not be deleted`);
+  }
   return data;
 }
